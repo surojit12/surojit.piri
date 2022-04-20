eac9c57f677488c514cb3d0c0ea9aa607fbce2b5
sabiha-thorium <sabiha.khan@functionup.org>
2022-04-20 16:28:16 +0530

    request and response headers

--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -1,22 +1,8 @@
 
-const mid1= function ( req, res, next) {
-    req.falana= "hi there. i am adding something new to the req object"
-    console.log("Hi I am a middleware named Mid1")
-    next()
-}
-
-const mid2= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid2")
-    next()
-}
-
-const mid3= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid3")
-    next()
-}
 
 const mid4= function ( req, res, next) {
     console.log("Hi I am a middleware named Mid4")
+    //counter
     next()
 }
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,19 +2,12 @@ const express = require('express');
 const router = express.Router();
 // const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
-const commonMW = require ("../middlewares/commonMiddlewares")
+//const BookController= require("../controllers/bookController")
+
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
-
-
-
-
-router.post("/createBook", BookController.createBook  )
-
-
 
 
 // router.post("/createUser", UserController.createUser  )
@@ -48,7 +41,8 @@ router.post("/createBook", BookController.createBook  )
 
 
 
-router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
+router.get("/basicRoute", UserController.basicCode)
+router.post('/create-a-user', UserController.createAUser)
 
 
 
